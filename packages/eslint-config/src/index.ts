238bee846640cf48747d19fc809a9fd730fedb23
Greg Kimmy <greg.kimmy@gmail.com>
2025-06-10 21:47:33 -0500

    initial commit

---
+++ packages/eslint-config/src/index.ts
@@ -0,0 +1,173 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintjs from '@eslint/js';
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import perfectionist from 'eslint-plugin-perfectionist';
+import type { Linter } from 'eslint';
+
+const compat = new FlatCompat({});
+
+export default {
+	configs: {
+		flat: [
+			{
+				files: ['**/*.{js,mjs,cjs,ts,mts}'],
+				languageOptions: {
+					globals: globals.node,
+					parser: tsParser,
+					parserOptions: {
+						projectService: true,
+						tsconfigRootDir: import.meta.dirname,
+					}					
+				}
+			},
+			eslintjs.configs.recommended,
+			stylistic.configs['recommended'],
+			perfectionist.configs['recommended-natural'],
+			...tseslint.configs.recommended,
+			{
+				rules: {
+					'no-debugger': 'warn',
+					'no-throw-literal': 'error',
+					'prefer-const': ['error', { destructuring: 'all' }],
+					'prefer-rest-params': 'off',
+					'perfectionist/sort-classes': [
+						'error',
+						{
+							order: 'asc',
+							type: 'natural',
+							groups:[
+								'index-signature',
+								'static-property',
+								'static-block',
+								['protected-property', 'protected-accessor-property'],
+								['private-property', 'private-accessor-property'],
+								['property', 'accessor-property'],
+								'constructor',
+								['static-method', 'protected-method', 'private-method', 'method', 'get-method', 'set-method'],
+								'unknown',
+							]
+						}
+					],
+					'perfectionist/sort-intersection-types': 'off',
+					'@stylistic/block-spacing': 'error',
+					'@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+					'@stylistic/comma-dangle': ['error', 'never'],
+					'@stylistic/comma-spacing': ['error', { before: false, after: true }],
+					'@stylistic/dot-location': ['error', 'property'],
+					'@stylistic/function-call-spacing': ['error', 'never'],
+					'@stylistic/indent': [
+						'error',
+						'tab',
+						{
+							ignoredNodes: ['ConditionalExpression'],
+							SwitchCase: 1
+						}
+					],
+					'@stylistic/indent-binary-ops': ['error', 'tab'],
+					'@stylistic/member-delimiter-style': [
+						'error',
+						{
+							multiline: {
+								delimiter: 'comma',
+								requireLast: false
+							},
+							singleline: {
+								delimiter: 'comma',
+								requireLast: false
+							}
+						}
+					],
+					'@stylistic/no-extra-semi': 'off',
+					'@stylistic/no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+					'@stylistic/no-tabs': ['error', { allowIndentationTabs: true }],
+					'@stylistic/no-trailing-spaces': 'error',
+					'@stylistic/no-whitespace-before-property': 'error',
+					'@stylistic/quote-props': ['error', 'consistent'],
+					'@stylistic/quotes': ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
+					'@stylistic/semi-spacing': 'error',
+					'@stylistic/semi': ['error', 'always'],
+					'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+					'@typescript-eslint/no-explicit-any': ['off'],
+					'@typescript-eslint/no-restricted-types': 'warn',
+					'@typescript-eslint/no-unused-vars': ['warn', { 'caughtErrors': 'none', 'ignoreRestSiblings': true }],
+					"@typescript-eslint/no-unnecessary-condition": "error",
+					'@typescript-eslint/naming-convention': [
+						'error',
+						{
+							selector: 'variable',
+							types: ['boolean'],
+							format: ['StrictPascalCase'],
+							filter: {
+								regex: '^(changed|done|found|result|retry|waiting|(.+)Changed|(.+)Updated)$',
+								match: false
+							},
+							prefix: ['bool', 'can', 'did', 'has', 'is', 'should', 'use', 'was', 'will']
+						},
+						{
+							selector: 'variable',
+							types: ['boolean'],
+							format: ['strictCamelCase'],
+							filter: {
+								regex: '^((.+)Changed|(.+)Updated)$',
+								match: true
+							},
+							suffix: ['Changed', 'Updated']
+						},
+						{
+							selector: 'variable',
+							types: ['function'],
+							modifiers: ['exported', 'global'],
+							format: ['StrictPascalCase']
+						},
+						{
+							selector: 'variable',
+							types: ['array', 'boolean', 'number', 'string'],
+							modifiers: ['exported', 'global'],
+							format: ['UPPER_CASE']
+						},
+						{
+							selector: ['class', 'enum', 'interface', 'typeAlias'],
+							format: ['StrictPascalCase']
+						},
+						{
+							selector: 'typeParameter',
+							format: ['StrictPascalCase'],
+							filter: {
+								regex: '^(T|U|V)$',
+								match: false
+							},
+							prefix: ['T']
+						},
+						{
+							selector: 'classProperty',
+							modifiers: ['private'],
+							leadingUnderscore: 'require',
+							format: ['strictCamelCase']
+						},
+						{
+							selector: 'classProperty',
+							modifiers: ['public'],
+							leadingUnderscore: 'forbid',
+							filter: {
+								regex: '^(_id)$',
+								match: false
+							},
+							format: ['strictCamelCase']
+						},
+						{
+							selector: 'classMethod',
+							filter: {
+								regex: '^(toJSON)$',
+								match: false
+							},
+							format: ['strictCamelCase']
+						}
+					]
+				}
+			}
+		] as Linter.Config
+	}
+};
